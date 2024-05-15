55f2fe386caca6be84a3b21c13d0bd5f99ed8a08
Atul Patare <atulpatare99@gmail.com>
2024-05-15 18:44:09 +0530

    Ref: Remove unused code | Add anon sign in (#2)
    * chore(env): web setup with vite-react
    
    * ci: npm causing issues
    
    * refactor(env): remove unused codes | storage schema and apis
    
    * feat(auth): add anon sign in api

--- backend/src/utils/responses.ts
+++ backend/src/utils/responses.ts
@@ -1,8 +1,7 @@
 import * as hapi from '@hapi/hapi'
 import { AuthAPIResponseCodes } from 'src/auth/types'
-import { FileAPIResponseCodes } from 'src/storage/types'
 
-export type Code = AuthAPIResponseCodes | FileAPIResponseCodes | 'unknown-error'
+export type Code = AuthAPIResponseCodes | 'unknown-error'
 
 export const data = (h: hapi.ResponseToolkit, data: object): hapi.ResponseObject => {
   return h.response(data).code(200)
