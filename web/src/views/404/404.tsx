8975323093cbf4f9ef8e8e3ad2af62145e821487
atul <atulpatare99@gmail.com>
2024-05-16 22:59:03 +0530

    refactor(env): move components separately from page

---
+++ web/src/views/404/404.tsx
@@ -0,0 +1,13 @@
+import { Container, Flex, Text } from "@radix-ui/themes";
+
+export const FourOFour = () => {
+  return (
+    <Container p="2" m="3" size="1">
+      <Flex justify="center">
+        <Text size="9" weight="bold">
+          404
+        </Text>
+      </Flex>
+    </Container>
+  );
+};
