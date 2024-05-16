c5cb81c4aeb4b6ce290634077c1d5054b18ee9e8
atul <atulpatare99@gmail.com>
2024-05-16 21:38:19 +0530

    feat(tutors): add profile page with basic info

--- web/src/pages/tutor_profile.tsx
+++ web/src/pages/tutor_profile.tsx
@@ -1,3 +1,13 @@
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Spinner,
+  Text,
+} from "@radix-ui/themes";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import * as tutorService from "../services/tutors";
@@ -18,5 +28,57 @@ export const TutorProfile = () => {
     }
   }, [params.tutorId]);
 
-  return <div>{JSON.stringify(profile)}</div>;
+  if (!profile) {
+    return (
+      <>
+        <Container p={"2"} size="1">
+          <Flex justify="center">
+            <Button disabled>
+              <Spinner loading></Spinner>
+              Loading
+            </Button>
+          </Flex>
+        </Container>
+      </>
+    );
+  }
+
+  return (
+    <Container p={"2"} size={"3"}>
+      <>
+        <Box width="auto" p={"1"}>
+          <Flex direction="column" gap="3" align="center" justify="center">
+            <Avatar
+              size="9"
+              mt="5"
+              src={profile.profile_picture}
+              radius="full"
+              fallback="T"
+            />
+            <Box style={{ textAlign: "center" }} mt="3">
+              <Text as="div" size="8" weight="bold">
+                {profile.first_name + " " + profile.last_name}
+              </Text>
+              <Flex gap="2" mt="2" justify="center">
+                <Badge color="red" size="3" radius="full">
+                  {"ATAR " + profile.atar}
+                </Badge>
+                <Badge color="yellow" size="3" radius="full">
+                  {profile.price}
+                </Badge>
+                <Badge
+                  color={profile.available ? "green" : "red"}
+                  size="3"
+                  radius="full"
+                >
+                  {profile.available ? "available" : "booked"}
+                </Badge>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
+      </>
+      <Flex></Flex>
+    </Container>
+  );
 };
