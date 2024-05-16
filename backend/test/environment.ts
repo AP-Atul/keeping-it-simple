15b86c1ef5e966df1018697a9127aecd707aaef6
atul <atulpatare99@gmail.com>
2024-05-16 11:22:05 +0530

    test(tutors): add basic search api tests

--- backend/test/environment.ts
+++ backend/test/environment.ts
@@ -1,10 +1,18 @@
+import { faker } from '@faker-js/faker'
 import * as hapi from '@hapi/hapi'
+import { mergeLeft } from 'ramda'
 import knex from '../src/db/db'
 import { init } from '../src/server'
+import * as tutorRepo from '../src/tutors/repo'
+import { SaveTutor, Tutor, TutorPricing } from '../src/tutors/types'
 
 export type TestServerEnvironment = {
   server: hapi.Server
   resetDB: () => Promise<void>
+}
+
+type MakeKeysOptional<T> = {
+  [K in keyof T]?: T[K]
 }
 
 let testEnv: TestServerEnvironment | undefined = undefined
@@ -14,6 +22,7 @@ const resetDB = async () => {
     delete from auth.refresh_tokens where true;
     delete from auth.sessions where true;
     delete from auth.users where true;
+    delete from public.tutors where true;
   `
   return knex.schema.raw(query)
 }
@@ -28,4 +37,33 @@ export const getTestEnv = async (): Promise<TestServerEnvironment> => {
     resetDB: resetDB
   }
   return testEnv
+}
+
+export const createSampleTutor = async (
+  entity?: MakeKeysOptional<Tutor>
+): Promise<Tutor> => {
+  const sample: SaveTutor = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    atar: faker.number.int({ min: 0, max: 100 }),
+    available: true,
+    bio: faker.lorem.sentence(),
+    postcode: faker.location.zipCode(),
+    price: faker.helpers.arrayElement<TutorPricing>(['executive', 'gold', 'premium']),
+    school: faker.helpers.arrayElement<string>([
+      'Primary School',
+      'Cambridge',
+      'Melbourne Uni',
+      'Harvard',
+      'Pune Uni',
+      'NASA Internation',
+      'Indian Institue'
+    ])
+  }
+  if (entity) {
+    const updated = mergeLeft(entity, sample)
+    console.log('updated', updated)
+    return (await tutorRepo.save(updated)) as Tutor
+  }
+  return (await tutorRepo.save(sample)) as Tutor
 }
---
+++ backend/test/integration/tutors.spec.ts
@@ -0,0 +1,50 @@
+import { expect } from 'chai'
+import { Tutor } from 'src/tutors/types'
+import { TestServerEnvironment, createSampleTutor, getTestEnv } from '../environment'
+
+let env: TestServerEnvironment
+const signin = async () => {
+  const response = await env.server.inject({
+    method: 'post',
+    url: '/auth/anon-signin'
+  })
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return response.result as any
+}
+
+beforeEach(async () => {
+  env = await getTestEnv()
+  await env.resetDB()
+})
+
+// todo: add more tests for each param
+describe('tutor search api', () => {
+  it('should list tutors with simple query', async () => {
+    const auth = await signin()
+    const tutor = await createSampleTutor({ first_name: 'random' })
+    const response = await env.server.inject<Tutor[]>({
+      method: 'get',
+      url: `/tutors/search?query=ran`,
+      headers: {
+        authorization: auth.auth_token
+      }
+    })
+    expect(response.statusCode).eq(200)
+    expect(response.result![0].id).eq(tutor.id)
+  })
+  it('should list tutors with simple query and price', async () => {
+    const auth = await signin()
+    const tutor = await createSampleTutor({ first_name: 'random', price: 'gold' })
+    await createSampleTutor({ price: 'premium' })
+    const response = await env.server.inject<Tutor[]>({
+      method: 'get',
+      url: `/tutors/search?query=ran&price=gold`,
+      headers: {
+        authorization: auth.auth_token
+      }
+    })
+    expect(response.statusCode).eq(200)
+    expect(response.result).length(1)
+    expect(response.result![0].id).eq(tutor.id)
+  })
+})
