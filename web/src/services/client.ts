38d955dae60556e2f167d8ecdac10a6e3dd140c2
Atul Patare <atulpatare99@gmail.com>
2024-05-17 11:50:14 +0530

    Bug: Auth token issue (#16)
    * fix(auth): fix intercept and sign in issue, add interceptor to set header
    
    * fix(auth): remove unnecessary manual sign in

--- web/src/services/client.ts
+++ web/src/services/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { signInAnon } from "./auth";
 
 const client = () => {
   const defaultOptions = {
@@ -11,12 +12,27 @@ const client = () => {
   // Create instance
   const instance = axios.create(defaultOptions);
 
-  // Set the AUTH token for any request
+  // interceptor to set auth token
   instance.interceptors.request.use(function (config) {
     const token = localStorage.getItem("token");
     config.headers.Authorization = token ? `Bearer ${token}` : "";
     return config;
   });
+
+  // interceptor to get auth token.. for anon user
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (error.response && error.response.status === 401) {
+        const authToken = await signInAnon();
+        const originalReq = error.config;
+        axios.defaults.headers.common.Authorization = `Bearer ${authToken}`;
+        originalReq.headers.Authorization = `Bearer ${authToken}`;
+        return axios(originalReq);
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return instance;
 };
